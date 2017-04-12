aa46568d44c9e6fbe9deb59d0558e0e86cfc41b9
Yu-Ting, Cheng <fishtoyo@gmail.com>
2017-04-12 15:39:44 +0800

    Add assets folder

---
+++ assets/js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function(e) {
+
+  // // Menu Button
+  // $('#header .menuBtn').click(function(){
+  //   $(this).toggleClass('open');
+  //   $('#nav').toggleClass('open', 4000);
+  // });
+
+  //Banner
+  // $(window).on('load resize orientation', function() {
+  //   $('#banner').height($(this).height());
+  // });
+
+});
